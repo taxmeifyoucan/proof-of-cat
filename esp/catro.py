ceeb302333b22310ad3c4337986ba153a972e247
Honza S. Copak <bonum.comunae@volny.cz>
2023-02-26 09:49:22 +0100

    refactoring - hw components

--- esp/catro.py
+++ esp/catro.py
@@ -1,53 +1,40 @@
 from utime import sleep, sleep_ms, ticks_ms
 import utime as time
+from sys import exit
 import json
 import random
-from machine import Pin, I2C
 import network
 import uasyncio
 import ubinascii
-from web_server import Nanoweb
-from mpu6500 import MPU6500
-from neopixel import NeoPixel
 from wifi_connect import WiFiConnect
+from web_server import Nanoweb
 from entropy_lib import MAX_SIZE, TARGET_ENTROPY, add_entropy, measure_entropy
-from sys import exit
+from components import init_rgb_led, init_gyro, get_data
+
+
 
 DEBUG = False
+LED_INTENSITY = 128
 
 print(f"MAX_SIZE: {MAX_SIZE}")
 print(f"TARGET_ENTROPY: {TARGET_ENTROPY}")
 print("-"*30)
 
-
-class StatusLed(NeoPixel):
-    _last_state = (0,0,0)
-
-    def show_led(self, color, force=False):
-        if self._last_state == color and not force:
-            return
-
-        self.fill(color)
-        self.write()
-        self._last_state = color
-        
-
 print("Starting Catropy")
 
-led = StatusLed(Pin(8), 1)
+
+led = init_rgb_led()
 
 for i in range(10):
-    led.show_led((200,0,0))
+    led.show_led((LED_INTENSITY,0,0))
     sleep_ms(100)
-    led.show_led((0,200,0))
+    led.show_led((0,LED_INTENSITY,0))
     sleep_ms(100)
 
-
-
-i2c = I2C(0, sda=Pin(0), scl=Pin(1))
-mpu = MPU6500(i2c)
+mpu = init_gyro()
 naw = Nanoweb()
 wlan = network.WLAN(network.STA_IF)
+
 
 def connect_wifi():
     if not wlan.isconnected():
@@ -58,18 +45,19 @@ def connect_wifi():
             sleep(2)
             ip_addr = net.ifconfig()[0]
             if ip_addr == "0.0.0.0":
-                led.show_led((200,0,0))
+                led.show_led((LED_INTENSITY,0,0))
                 print("The wifi doesn't seem not be connected corretly, please check your settings and run the program again.")
             else:
                 print("IP address:",ip_addr)
-                led.show_led((0,200,0))
+                led.show_led((0,LED_INTENSITY,0))
         except:
             print("The wifi did not connect, please check your settings and run the program again.")
-            led.show_led((200,0,0))
+            led.show_led((LED_INTENSITY,0,0))
      
 connect_wifi()
 if not wlan.isconnected():
     exit()
+
 
 #deactivate to save battery
 #wlan.active(False)
@@ -83,34 +71,16 @@ def get_uptime():
     return ('{:02d}h {:02d}m {:02d}z'.format(uptime_h, uptime_m, uptime_s))
 
 
-# Read values from sensors
-def get_data():
-    gyro = mpu.gyro
-    accel = mpu.acceleration
-    if DEBUG:
-        print(gyro[0],gyro[1],gyro[2], accel[0], accel[1], accel[2])
-    else:
-        print(".",end="")
-    
-    ax=accel[0] #vals["AcX"]
-    ay=accel[1] #vals["AcY"]
-    az=accel[2] #vals["AcZ"]
-    gx=gyro[0] #vals["GyX"]
-    gy=gyro[1] #vals["GyY"]
-    gz=gyro[2] #vals["GyZ"]
-    return (ax, ay, az, gx, gy, gz)
-
-
 def rng():
     global entropy_str
-    led.show_led((0,200,0))
+    led.show_led((0,LED_INTENSITY,0))
     i=0
     if not "entropy_str" in globals():
         entropy_str = ""
         while True:
             sample_period = int(random.uniform(500, 1100)) 
             sleep_ms(sample_period)
-            data = get_data()
+            data = get_data(mpu)
             entropy_str = add_entropy(mpu, entropy_str, data)
             i+=1
             if i % 5 == 0:
@@ -122,11 +92,11 @@ def rng():
         for i in range(4):
             sample_period = int(random.uniform(100, 400))
             sleep_ms(sample_period)
-            data = get_data()
+            data = get_data(mpu)
             entropy_str = add_entropy(mpu, entropy_str, data)
         print("Current entropy: {} bits".format(measure_entropy(entropy_str)))
         print("Current lenght:", len(entropy_str))
-    led.show_led((0,0,200))
+    led.show_led((0,0,LED_INTENSITY))
     return entropy_str
 
 print("Starting randomness generation")
@@ -160,6 +130,5 @@ loop.create_task(naw.run())
 connect_wifi()
 if not wlan.isconnected():
     exit()
-led.show_led((0,0,200))
+led.show_led((0,0,LED_INTENSITY))
 loop.run_forever()
-
